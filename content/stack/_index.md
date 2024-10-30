3f9ab465b7a4240fa17b5468c6314b5112896f52
Brage Sekse Aarset <brage@bjerk.io>
2024-10-30 12:26:46 +0200

    feat: hugo

---
+++ content/stack/_index.md
@@ -0,0 +1,7 @@
+---
+title: Stack
+description: Tools I use everyday
+---
+
+
+# Hey
---
+++ public/main.js
@@ -0,0 +1,76 @@
+(() => {
+  // <stdin>
+  var sidebar = document.querySelector("aside.sidebar");
+  var menuTrigger = document.querySelector("button.menu-trigger");
+  var menuTriggerClose = document.querySelector("button.menu-trigger-close");
+  var menuOpacity = document.querySelector("div.menu-overlay");
+  var toggleSidebar = () => {
+    if (sidebar.classList.contains("!translate-x-0")) {
+      sidebar.classList.remove("!translate-x-0");
+      menuOpacity.classList.add("hidden");
+    } else {
+      sidebar.classList.add("!translate-x-0");
+      menuOpacity.classList.remove("hidden");
+    }
+  };
+  menuTrigger.addEventListener("click", toggleSidebar);
+  menuTriggerClose.addEventListener("click", toggleSidebar);
+  menuOpacity.addEventListener("click", toggleSidebar);
+  var scrollElement = document.querySelector(".scroll-area");
+  var scrollElementStateKey = "ScrollElementPosition";
+  window.onbeforeunload = function () {
+    if (!scrollElement) return;
+    const scrollPos = scrollElement.scrollTop;
+    if (scrollPos) {
+      localStorage.setItem(scrollElementStateKey, scrollPos);
+    }
+  };
+  window.onload = function () {
+    const scrollPos = localStorage.getItem(scrollElementStateKey);
+    localStorage.removeItem(scrollElementStateKey);
+    if (scrollElement) {
+      scrollElement.scrollTop = scrollPos;
+    }
+  };
+  var darkModeToggle = document.querySelector(".dark-mode-toggle");
+  var darkModeStateKey = "DarkMode";
+
+  var systemIsDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+  var isDark = false;
+  if (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  ) {
+    // dark mode
+    isDark = true;
+  }
+
+  window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", (event) => {
+      isDark = event.matches ? true : false;
+      if (isDark) {
+        document.documentElement.classList.add("dark");
+        localStorage.setItem(darkModeStateKey, true);
+      } else {
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem(darkModeStateKey, false);
+      }
+    });
+
+  isDark = JSON.parse(localStorage.getItem(darkModeStateKey) || systemIsDark);
+
+  if (isDark) {
+    document.documentElement.classList.add("dark");
+  }
+  darkModeToggle.addEventListener("click", () => {
+    if (document.documentElement.classList.contains("dark")) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem(darkModeStateKey, false);
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem(darkModeStateKey, true);
+    }
+  });
+})();
